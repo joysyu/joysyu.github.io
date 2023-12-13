293a0c7b3ecf42b9400d3e29345f9482e79b3ca8
Joy Yu <joy.yu@frontapp.com>
2023-12-13 12:24:12 -0800

    try

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ window.addEventListener(
   },
   false
 );
+
+const frame = document.getElementById("frame");
+const button = document.getElementById("button");
+button.onclick = () => {
+  frame.contentWindow.postMessage(
+    {
+      api: "opencti_dialpad",
+      version: "1.0",
+      method: "initiate_call",
+      payload: {
+        enable_current_tab: true,
+        phone_number: "+15103669944",
+      },
+    },
+    "*"
+  );
+};
