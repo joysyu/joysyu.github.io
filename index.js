28e7589ace4bb683b5ddc7ba53c9f94f0b7d4201
Joy Yu <joy.yu@Joy-Yus-MBP.local.meter>
2024-01-23 16:57:20 -0800

    dfjfd

--- index.js
+++ index.js
@@ -1,43 +1,66 @@
-import { Notification } from 'electron';
+// main.js
 
-// window.addEventListener(
-//   "message",
-//   (event) => {
-//     console.log("received message at ", Date.now());
-//     if (event.data.source !== "react-devtools-content-script") {
-//       console.log(event);
-//     }
-//     // const div = document.getElementById("abc");
-//     // div.innerHTML = event.data;
-//   },
-//   false
-// );
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, Notification } = require('electron')
+const path = require('node:path')
 
-// function initiateCall() {
-//   console.log("clicked!");
-//   const frame = document.getElementById("frame");
-//   console.log(frame);
-//   frame.contentWindow.postMessage(
-//     {
-//       api: "opencti_dialpad",
-//       version: "1.0",
-//       method: "initiate_call",
-//       payload: {
-//         enable_current_tab: true,
-//         phone_number: "+15103669944",
-//       },
-//     },
-//     "https://dialpad.com"
-//   );
-// }
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+        nodeIntegration: true,
+        contextIsolation: false,
+    }
+  })
 
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+}
+
+const NOTIFICATION_TITLE = 'Basic Notification'
+const NOTIFICATION_BODY = 'Notification from the Main process'
+
+function showNotification () {
+  new Notification({ title: NOTIFICATION_TITLE, body: NOTIFICATION_BODY }).show()
+}
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+}).then(showNotification)
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
 function onClick() {
   const notificationOptions = {
-    title: "HELLO!!!!!",
-    body: "this is the body!!!",
+    title: "title",
+    body: "body",
   };
 
   const notification = new Notification(notificationOptions);
+
   notification.show();
-  console.log(notification);
+
+  console.log("notification: ", notification);
 }
