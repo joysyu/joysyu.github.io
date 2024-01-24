f6873e22111401220b13e1c1a6e5cba24c52985d
Joy Yu <joy.yu@Joy-Yus-MBP.local.meter>
2024-01-23 16:25:30 -0800

    notif

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
-window.addEventListener(
-  "message",
-  (event) => {
-    console.log("received message at ", Date.now());
-    if (event.data.source !== "react-devtools-content-script") {
-      console.log(event);
-    }
-    // const div = document.getElementById("abc");
-    // div.innerHTML = event.data;
-  },
-  false
-);
+const {Notification} = require('electron');
 
-function initiateCall() {
-  console.log("clicked!");
-  const frame = document.getElementById("frame");
-  console.log(frame);
-  frame.contentWindow.postMessage(
-    {
-      api: "opencti_dialpad",
-      version: "1.0",
-      method: "initiate_call",
-      payload: {
-        enable_current_tab: true,
-        phone_number: "+15103669944",
-      },
-    },
-    "https://dialpad.com"
-  );
-}
+// window.addEventListener(
+//   "message",
+//   (event) => {
+//     console.log("received message at ", Date.now());
+//     if (event.data.source !== "react-devtools-content-script") {
+//       console.log(event);
+//     }
+//     // const div = document.getElementById("abc");
+//     // div.innerHTML = event.data;
+//   },
+//   false
+// );
+
+// function initiateCall() {
+//   console.log("clicked!");
+//   const frame = document.getElementById("frame");
+//   console.log(frame);
+//   frame.contentWindow.postMessage(
+//     {
+//       api: "opencti_dialpad",
+//       version: "1.0",
+//       method: "initiate_call",
+//       payload: {
+//         enable_current_tab: true,
+//         phone_number: "+15103669944",
+//       },
+//     },
+//     "https://dialpad.com"
+//   );
+// }
+
+const notificationOptions = {
+  title: "HELLO!!!!!",
+  body: "this is the body!!!",
+};
+
+const notification = new Notification(notificationOptions);
+
+notification.show();
