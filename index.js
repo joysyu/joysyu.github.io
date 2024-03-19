767b1037e5d66b11ff533662554cab203f28737e
Joy Yu <joy.yu@Joy-Yus-MBP.local.meter>
2024-03-19 15:27:37 -0700

    df

--- index.js
+++ index.js
@@ -68,3 +68,56 @@ function onClick() {
 
   showNotification();
 }
+
+const btn = document.getElementById('abc');
+
+function doThingsWithCookies() {
+  document.cookie = "foo=bar"; // set a cookie
+}
+
+async function handleCookieAccess() {
+  if (!document.hasStorageAccess) {
+    // This browser doesn't support the Storage Access API
+    // so let's just hope we have access!
+    doThingsWithCookies();
+  } else {
+    const hasAccess = await document.hasStorageAccess();
+    if (hasAccess) {
+      // We have access to third-party cookies, so let's go
+      doThingsWithCookies();
+    } else {
+      // Check whether third-party cookie access has been granted
+      // to another same-site embed
+      try {
+        const permission = await navigator.permissions.query({
+          name: "storage-access",
+        });
+
+        if (permission.state === "granted") {
+          // If so, you can just call requestStorageAccess() without a user interaction,
+          // and it will resolve automatically.
+          await document.requestStorageAccess();
+          doThingsWithCookies();
+        } else if (permission.state === "prompt") {
+          // Need to call requestStorageAccess() after a user interaction
+          btn.addEventListener("click", async () => {
+            try {
+              await document.requestStorageAccess();
+              doThingsWithCookies();
+            } catch (err) {
+              // If there is an error obtaining storage access.
+              console.error(`Error obtaining storage access: ${err}.
+                            Please sign in.`);
+            }
+          });
+        } else if (permission.state === "denied") {
+          // User has denied third-party cookie access, so we'll
+          // need to do something else
+        }
+      } catch (error) {
+        console.log(`Could not access permission state. Error: ${error}`);
+        doThingsWithCookies(); // Again, we'll have to hope we have access!
+      }
+    }
+  }
+}
